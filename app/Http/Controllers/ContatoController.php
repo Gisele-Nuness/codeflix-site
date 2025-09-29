a98907735b0811ce07bcdf4322f3b6bb4ed840f3
Gisele-Nuness <giseleppuca2008@hotmail.com>
2025-09-29 17:31:11 -0300

    Auto stash before merge of "feature-branch-gisele" and "upstream/main"

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -32,10 +32,16 @@ class ContatoController extends Controller
     public function storeApi(Request $request)
     {
         $data = $request->validate([
-            'nome'     => ['required','string','max:255'],
-            'email'    => ['required','email','max:255'],
-            'mensagem' => ['required','string','max:5000'],
+            'nome'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'email', 'max:255'],
+            'mensagem' => ['required', 'string', 'max:5000'],
+            'caminho_foto'  => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
         ]);
+        $path = '';
+        if ($request->File('caminho_foto')) {
+            $path = $request->file('caminho_foto')->store('images', 'public');
+        }
+        $data['caminho_foto'] = $path;
 
         $contato = Contato::create($data);
 
@@ -49,9 +55,9 @@ class ContatoController extends Controller
     public function store(Request $request)
     {
         $data = $request->validate([
-            'nome'          => ['required','string','max:255'],
-            'email'         => ['required','email','max:255'],
-            'mensagem'      => ['required','string','max:5000'],
+            'nome'          => ['required', 'string', 'max:255'],
+            'email'         => ['required', 'email', 'max:255'],
+            'mensagem'      => ['required', 'string', 'max:5000'],
             'caminho_foto'  => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
         ]);
         $path = '';
@@ -71,9 +77,9 @@ class ContatoController extends Controller
     public function updateApi(Request $request, string $id)
     {
         $validarDados = $request->validate([
-            'nome'     => ['sometimes','string','min:3','max:255'],
-            'email'    => ['sometimes','email','max:255'],
-            'mensagem' => ['sometimes','string','max:5000'],
+            'nome'     => ['sometimes', 'string', 'min:3', 'max:255'],
+            'email'    => ['sometimes', 'email', 'max:255'],
+            'mensagem' => ['sometimes', 'string', 'max:5000'],
         ]);
 
         $contato = Contato::findOrFail($id);
